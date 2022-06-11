5c1f14c498fbe2f2aba656787da5123c092986d2
joelbit33 <j.oskar.nilsson@gmail.com>
2022-06-11 17:57:44 +0200

    fixed responsive container

---
+++ script.js
@@ -0,0 +1,9 @@
+let container = document.querySelector('#container');
+
+
+for (let i = 0; i < 16*16; i++) {
+  let squareDiv = document.createElement('div');
+  container.appendChild(squareDiv);
+}
+
+
